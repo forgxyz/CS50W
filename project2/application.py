6cbae81243f936dca7933c639b0978e71b01fb18
forgxyz <jmfxyz@pm.me>
2020-04-07 23:38:19 -0400

    nothing

--- project2/application.py
+++ project2/application.py
@@ -1,28 +1,42 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask_socketio import SocketIO, emit
-
-from helpers import *
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") # ???
 socketio = SocketIO(app)
 
 
 @app.route("/")
-@login_required
 def index():
-    name = session.get("username")
-    return render_template("index.html", name=name)
+    return render_template("index.html")
 
 
 @app.route("/home")
 def home():
-    return("THIS IS THE HOMEPAGE")
+    return "THIS IS THE HOMEPAGE"
 
 
-@app.route("/login")
-def login():
-    return render_template("index.html")
+@app.route("/set_name", methods=['GET', 'POST'])
+def set_name():
+    return f"""
+          <form id="displayname_form">
+            <div class="form-group">
+              <label for="username">What should we call you?</label>
+              <input type="text" class="form-control" id="username" name="username" aria-describedby="Username">
+            </div>
+            <button type="button" class="btn btn-primary" id="displayname">Submit</button>
+          </form>
+          <div id="test"></div>
+    """
+
+
+@app.route("/test")
+def test():
+    return "TESTING ROUTE"
+
+if __name__ == "__main__":
+    # app.run(debug=True, host="0.0.0.0")
+    socketio.run(app)
